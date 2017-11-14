a3c899f8489d3c2b9096f494b665de46f1ef1673
David <doneill39@gmail.com>
2017-11-14 21:33:36 +0000

    Update registration.php

--- application/views/registration.php
+++ application/views/registration.php
@@ -25,7 +25,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                                     <li><a href="<?php echo site_url('main/index') ?>">Home</a></li>
+                                     <li><a href="<?php echo site_url('main/index') ?>"><li><a href="<?php echo site_url('main/index') ?>"><span class="glyphicon glyphicon-home"></span> Home</a></li> Home</a></li>
 									<li><a href="<?php echo site_url('user/register') ?>">Register</a></li>
 									<li><a href="<?php echo site_url('user/login') ?>">Log in</a></li>
 									<li><a href="<?php echo base_url().'medicines/show_medicines' ?>">Available medicines</a></li>
