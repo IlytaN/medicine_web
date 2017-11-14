3705dc7d99fb5143caa660ab72cca06c25190a42
David <doneill39@gmail.com>
2017-11-14 12:12:23 +0000

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -14,6 +14,25 @@
       margin: 5% auto;
     }
   </style>
+   <nav class="navbar navbar-inverse" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand">Medicine web</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                                     <li><a href="<?php echo site_url('main/index') ?>">Home</a></li>
+									<li><a href="<?php echo site_url('user/register') ?>">Register</a></li>
+									<li><a href="<?php echo site_url('user/login') ?>">Log in</a></li>
+									<li><a href="<?php echo base_url().'medicines/show_medicines' ?>">Available medicines</a></li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
 </head>
 <body>
   <div class="wrapper">
