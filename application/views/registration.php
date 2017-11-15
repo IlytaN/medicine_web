af9e01ae3841fd77e8c2f78a7b158d3176c76abd
David <doneill39@gmail.com>
2017-11-15 15:36:55 +0000

    Update registration.php

--- application/views/registration.php
+++ application/views/registration.php
@@ -25,7 +25,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                                     <li><a href="<?php echo site_url('main/index') ?>"><li><a href="<?php echo site_url('main/index') ?>"><span class="glyphicon glyphicon-home"></span> Home</a></li> Home</a></li>
+                                     <li><a href="<?php echo site_url('main/index') ?>"><span class="glyphicon glyphicon-home"></span> Home</a></li> 
 									<li><a href="<?php echo site_url('user/register') ?>">Register</a></li>
 									<li><a href="<?php echo site_url('user/login') ?>">Log in</a></li>
 									<li><a href="<?php echo base_url().'medicines/show_medicines' ?>">Available medicines</a></li>
@@ -39,7 +39,7 @@
     <div class="container">
       <div class="row">
         <div class="form-box">
-          <div class="panel panel-primary">
+          <div class="panel panel-success">
             <div class="panel-heading text-center">
               <h3>Register</h3>
             </div>
@@ -54,20 +54,21 @@
                 <div class="row">
                   <div class="col-sm-6">
                     <div class="form-group">
-                                    <label class="control-label" for="fname">First Name</label>
-                                        <div >
+                                    <label class="control-label" for="fname"><span class="glyphicon glyphicon-user"></span>First Name</label>
+                                        <div>
                                             <input type="text" class="form-control" id="fname" name="fname" placeholder="First Name" required="">
                                             <span class="text-danger"><?php echo form_error('fname'); ?></span>
                                         </div>
-                                </div>
-                  </div>
-                  <div class="col-sm-6">
-                    <div class="form-group">
+                                
+            
                                 <label class="control-label" for="pswd"><span class="glyphicon glyphicon-envelope"></span>Email</label>
                                     <div>
                                         <input type="email" class="form-control" id="email" name="email" placeholder="Email" required="">
                                         <span class="text-danger"><?php echo form_error('email'); ?></span>
                                     </div>
+                      </div>
+                                        </div>
+
                             </div>
 
                             <div class="form-group">
@@ -87,7 +88,7 @@
                             <div class="form-group">
                                 <div class="row">
                                     <div class="col-sm-offset-5 col-sm-3  btn-submit">
-                                        <button type="submit" class="btn btn-success">Register</button>
+                                        <button type="submit" class="btn"><b>Register</b></button>
                                     </div>
                                 </div>
                             </div>
