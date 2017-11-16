002a32350d705141e0c3e73030b3233313386da2
David <doneill39@gmail.com>
2017-11-16 18:46:04 +0000

    Update list.php

--- application/views/medicines/list.php
+++ application/views/medicines/list.php
@@ -4,24 +4,25 @@
       <?php
       foreach ($loggedUser as $row) {
         echo "<div class='col-md-3'>
-          <img class='img-thumbnail' src='https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m.jpg'>";
+          <img class='img-thumbnail' src='https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png'>";
         echo '<h3>'.$row['fname'].$row['lname'].'</h3></div>';
         echo "<div class='col-md-7'>
                 <p>".$row['description']."</p>
               </div>";
-        echo '<button class="btn btn-success"><a href="'.base_url().'index.php/medicines/editdescription">Edit</a></button>';
+        echo '<button class="btn btn-dark"><a href="'.base_url().'index.php/medicines/editdescription">Edit</a></button>';
       }
       ?>
   </div>
 
 <!-- personal medicine list -->
+    <div class="fixed-bottom medfont"> 
   <TABLE class="table table-bordered">
     <TR>
-      <TH>id</TH>
-      <TH>medicine id</TH>
-      <TH>name</TH>
-      <th>img</th>
-      <TH>description</TH>
+      <TH>ID</TH>
+      <TH>Medicine ID</TH>
+      <TH>Name</TH>
+      <th>Image</th>
+      <TH>Description</TH>
       <th></th>
     </TR>
     <?php
@@ -31,7 +32,7 @@
       echo '<td>'.$row['name'].'</td>';
       echo '<td><img class="img-thumbnail" style="max-width:200px" src="'.$row['img'].'"></td>';
       echo '<td>'.$row['description'].'</td>';
-      echo '<td><button class="btn btn-success"><a href="'.site_url('medicines/delete_medicine').'/'.
+      echo '<td><button class="btn btn-basic"><a href="'.site_url('medicines/delete_medicine').'/'.
   		$row['id'].'">Delete</a></button></td></tr>';
     }
     ?>
