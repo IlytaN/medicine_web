a1ce9357c6eef4f4aabebf3d1a5e62edb666dee6
David <doneill39@gmail.com>
2017-11-16 18:54:42 +0000

    Update header.php

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Love drugs</title>
+<title>MedicineWeb</title>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/mystyle.css');?>">
 <!-- Bootstrap Core CSS -->
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-</head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyBeLdYWFSGPZJcT_NERzJleOaNlQeLqgxo&callback=initMap"
+  type="text/javascript"></script>
+    </head>
 <body>
 	<nav class="navbar navbar-inverse" role="navigation">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <a class="navbar-brand">Medicine web</a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                                     <li><a href="<?php echo site_url('main/index') ?>"><li><a href="<?php echo site_url('main/index') ?>"><span class="glyphicon glyphicon-home"></span> Home</a></li> Home</a></li>
+                                     <li><a href="<?php echo site_url('main/index') ?>"><span class="glyphicon glyphicon-home"></span> Home</a></li>
 									<li><a href="<?php echo site_url('user/register') ?>">Register</a></li>
 									<li><a href="<?php
 												if (!empty($sess_id))
@@ -30,8 +32,6 @@
 									<li><a href="<?php echo site_url().'/medicines/show_medicines' ?>">Available medicines</a></li>
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
     </nav>
 		<div class="container">
